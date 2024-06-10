38f3f668fa59ee2a6ec6780f33c996685bf5a807
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-06-10 10:41:52 +0800

    feat: added nav bar without funcs

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import Link from "next/link"
+import styles from "./NavBar.module.css"
+import { useState, FC, ReactNode } from "react"
+
+
+type NavBarItem = {
+    path: string,
+    label: string,
+    icon: ReactNode
+}
+
+const NavBar: FC<{ navBarItems: NavBarItem[] }> = ({navBarItems}) => {
+
+  const [selectedIndex, setselectedIndex] = useState(0)
+
+
+  return (
+    <>
+    <div className={`${styles.nav_bar} ${styles.nav_bar_hidden}`}>
+    {
+        navBarItems.map((navBarItem,index) =>{
+            return(
+                <li 
+                key={index}
+                id={"nav_bar-button-" + index}
+                className={`${styles.nav_bar_button}`}
+                onClick={(e) => {
+                    setselectedIndex(index)
+                }}
+                
+                >
+                <div id={"nav_bar-selected-" + index} className={selectedIndex === index? `${styles.nav_bar_selected}`: `${styles.nav_bar_selected} ${styles.no_display}`} title={navBarItem.label}></div>
+                {navBarItem.icon}
+                </li>
+            )
+        })
+        }
+    </div>
+
+    <div className={`${styles.nav_bar}`}>
+        <ul className={`${styles.nav_bar_buttons}`}>
+            {
+            navBarItems.map((navBarItem,index) =>{
+                return(
+                    <li 
+                    key={index}
+                    id={"nav_bar-button-" + index}
+                    className={`${styles.nav_bar_button}`}
+                    onClick={(e) => {
+                        setselectedIndex(index)
+                    }}
+                    
+                    >
+                        <div id={"nav_bar-selected-" + index} className={selectedIndex === index? `${styles.nav_bar_selected}`: `${styles.nav_bar_selected} ${styles.no_display}`}></div>
+                        <div title={navBarItem.label}>
+                            {navBarItem.icon}
+                        </div>
+                    </li>
+                )
+            })
+            }
+        </ul>
+        <div className={`${styles.nav_bar_label}`}>
+        <div className={`${styles.nav_bar_title}`}>JM</div>
+        <div>@2024</div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+
+
+export default NavBar
