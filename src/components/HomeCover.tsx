8393c57c49c0058c20f4fc4b0152ec66196395e6
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-06-10 18:42:40 +0800

    feat: catch unwanted characters in deployment

--- src/components/HomeCover.tsx
+++ src/components/HomeCover.tsx
@@ -16,13 +16,13 @@ export function HomeCover() {
 
 
     <div className={styles.home_cover_description}>
-      Hey there! I'm Jomar P. Monreal, a Filipino enthusiastic 21st century literate with high adaptability and appreciation. As a versatile developer, I bring extensive experience in a wide range of programming languages and technologies.
+      <strong>Hey there!</strong> I&aposm Jomar P. Monreal, a Filipino enthusiastic 21st century literate with high adaptability and appreciation. As a versatile developer, I bring extensive experience in a wide range of programming languages and technologies.
       <ul>
         <li>
           <FaReact size={iconSize}/> <div className="padded"> <strong>MERN Stack:</strong> Full-stack development with MongoDB, Express.js, React, and Node.js.</div>
         </li>
         <li>
-          <SiNextdotjs size={iconSize}/> <div className="padded"> <strong>Next.js:</strong> High-performance and scalable web application development.</div>
+          <SiNextdotjs size={iconSize}/> <div className="padded"> <strong>Next.js:</strong> High-performance and scalable web application development with Typescript.</div>
         </li>
         <li>
           <SiFlutter size={iconSize}/> <div className="padded"> <strong>Flutter:</strong> Cross-platform mobile applications for iOS and Android.</div>
