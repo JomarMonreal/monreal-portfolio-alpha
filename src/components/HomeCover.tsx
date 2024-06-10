2568f05f8ad34b867e358902ca58de07f7bd0c98
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-06-10 12:51:06 +0800

    chore: updated skills description

--- src/components/HomeCover.tsx
+++ src/components/HomeCover.tsx
@@ -1,4 +1,7 @@
+import { FaJava, FaPython, FaReact } from "react-icons/fa";
 import styles from "./HomeCover.module.css";
+import { SiFlutter, SiNextdotjs } from "react-icons/si";
+import { iconSize } from "@/constants/iconConstants";
 
 export function HomeCover() {
   return <div className={styles.home_cover}>
@@ -13,17 +16,23 @@ export function HomeCover() {
 
 
     <div className={styles.home_cover_description}>
-      Hey there! I'm Jomar P. Monreal, a Filipino enthusiastic 21st century literate with high adaptability and
-      appreciation. As a versatile developer, I bring extensive experience in a wide range of programming languages and technologies.
+      Hey there! I'm Jomar P. Monreal, a Filipino enthusiastic 21st century literate with high adaptability and appreciation. As a versatile developer, I bring extensive experience in a wide range of programming languages and technologies.
       <ul>
         <li>
-          I specialize in building dynamic web applications using <strong>React</strong>, creating responsive and interactive user experiences.
-          Proficient in the <strong>MERN stack</strong> (MongoDB, Express.js, React, Node.js), I develop comprehensive full-stack solutions.
+          <FaReact size={iconSize}/> <div className="padded"> <strong>MERN Stack:</strong> Full-stack development with MongoDB, Express.js, React, and Node.js.</div>
         </li>
-        <li>I have developed this website using <strong>Next.js</strong>, showcasing my ability to create high-performance and scalable web applications.</li>
-        <li>My expertise in <strong>Flutter</strong> empowers me to develop cross-platform mobile applications that perform seamlessly on both iOS and Android devices.</li>
-        <li>My strong grasp of <strong>Java</strong> enables me to create efficient and robust applications, especially in developing games.</li>
-        <li>My proficiency in <strong>Python</strong> allows me to handle data analysis and statistics with ease.</li>
+        <li>
+          <SiNextdotjs size={iconSize}/> <div className="padded"> <strong>Next.js:</strong> High-performance and scalable web application development.</div>
+        </li>
+        <li>
+          <SiFlutter size={iconSize}/> <div className="padded"> <strong>Flutter:</strong> Cross-platform mobile applications for iOS and Android.</div>
+        </li>
+        <li>
+          <FaJava size={iconSize}/> <div className="padded"> <strong>Java:</strong> Efficient and robust applications, especially for game development.</div>
+        </li>
+        <li>
+          <FaPython size={iconSize}/> <div className="padded"> <strong>Python:</strong> Data analysis and statistics.</div>
+        </li>
       </ul>
       <p>With this comprehensive skill set, I am well-equipped to tackle diverse projects and deliver high-quality solutions that meet client needs and exceed expectations.</p>
     </div>
