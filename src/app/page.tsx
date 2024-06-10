da0810535964c4554bddd6e5cff85d15794aab00
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-06-10 12:51:39 +0800

    ui: some ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { HomeCover } from "../components/HomeCover";
 import { Bamboos } from "../components/Bamboos";
 import styles from "./page.module.css";
-import { iconColor } from "@/constants/iconConstants";
 import { ContactMeSection } from "../components/ContactMeSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 
