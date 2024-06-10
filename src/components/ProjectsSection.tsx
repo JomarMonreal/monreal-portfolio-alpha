3b66b9ff9ebe2b8f5f07d100760306b345677074
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-06-10 18:40:35 +0800

    chore: project section with popup

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -4,23 +4,33 @@ import { Project, convertToProjectsArray} from "@/types/types";
 import { ProjectsSectionCard } from "./ProjectsSectionCard";
 import projectsJSON from "../../public/json/projects.json"
 import { useState } from "react";
+import Popup from "./Popup";
+import { ProjectDetailScreen } from "../popup_screens/ProjectDetailScreen";
 
 export function ProjectsSection() {
   const [projects, setProjects] = useState<Project[]>(convertToProjectsArray(projectsJSON))
+  const [isPopupVisible, setIsPopupVisible] = useState(false)
+  const [currrentProject, setCurrrentProject] = useState<Project>(projects[0]);
 
   return (
-    <div>
-      <h2>Projects</h2>
+    <>
+      <h2 id="project_section" style={{marginBottom: "1rem"}}>Projects</h2>
+      <p>Explore a selection of my featured works and innovations, showcasing a diverse range of projects and creative solutions.</p>
       {projects.map((project, index) => (
         <ProjectsSectionCard
           key={index}
           type={project.type}
           imagePath={project.imagePath}
           title={project.title}
-          description={project.description}
-          projectUrl={project.projectUrl}
+          description={project.description.split(".")[0]}
+          projectUrl={project.projectUrl} 
+          onViewClick={()=>{setIsPopupVisible(true); setCurrrentProject(project)}}
         />
       ))}
-    </div>
+      {isPopupVisible? <Popup onOutsideClick={()=>{setIsPopupVisible(false)}} borderRadius={"2rem"} width={"50vw"} height={"80vh"}>
+        <ProjectDetailScreen type={currrentProject.type} title={currrentProject.title} description={currrentProject.description} imageUrl={currrentProject.imageUrl} projectUrl={currrentProject.projectUrl}/>
+      </Popup>: null}
+    </>
   );
 }
+
--- src/components/ProjectsSectionCard.tsx
+++ src/components/ProjectsSectionCard.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import styles from "./ProjectsSection.module.css";
+import styles from "./ProjectsSectionCard.module.css";
 import { ProjectType } from "../types/types";
 
 interface ProjectSectionCardProps{
@@ -8,15 +8,16 @@ interface ProjectSectionCardProps{
   title: string,
   description: string,
   projectUrl: string,
+  onViewClick: ()=>void
 }
 
 export const ProjectsSectionCard:FC<ProjectSectionCardProps> = (props) => {
   return <div className={styles.projects_section_card} style={{ alignItems: "center" }}>
     <img className={styles.projects_section_image} src={props.imagePath} />
-    <div className="padded flexed" style={{ flexDirection: "column", height: "100%" }}>
+    <div className="padded flexed" style={{ flexDirection: "column", justifyContent:"center", height: "100%" }}>
       <h3 style={{ margin: 0 }}>{props.title}</h3>
       <p className={styles.projects_section_description}>{props.description}</p>
-      <button style={{ width: "auto" }}>View more details</button>
+      <button style={{ width: "auto" }} onClick={props.onViewClick}>View more details</button>
     </div>
   </div>;
 }
