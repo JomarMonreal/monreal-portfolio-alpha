0d544a490fcec360ea3d3596ca378c7a30cd3f03
JomarMonreal <jpmonreal2035@gmail.com>
2024-06-18 11:09:03 +0800

    chore: display projects by groups

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -13,22 +13,30 @@ export const ProjectsSection:FC<ProjectsSectionProps> = (props) => {
   const [isPopupVisible, setIsPopupVisible] = useState(false)
   const [currrentProject, setCurrrentProject] = useState<Project>(props.projects[0]);
 
+  const projectTypes = ["website","mobile","game"]
+
   return (
     <section id="project_section">
       <FloatingTitle text="Projects" tag="h2" className="text-6xl" />
       <p>Explore a selection of my featured works and innovations, showcasing a diverse range of projects and creative solutions.</p>
 
-      {props.projects.map((project, index) => (
-        <ProjectsSectionCard
-          key={index}
-          type={project.type}
-          imagePath={project.imagePath}
-          title={project.title}
-          description={project.description.split(".")[0]}
-          projectUrl={project.projectUrl} 
-          onViewClick={()=>{setIsPopupVisible(true); setCurrrentProject(project)}}
-        />
-      ))}
+      {
+        projectTypes.map((projectType)=>{
+          return (
+            props.projects.filter((project)=>projectType === project.type).map((project, index) => (
+              <ProjectsSectionCard
+                key={`${projectType}-${index}`}
+                type={project.type}
+                imagePath={project.imagePath}
+                title={project.title}
+                description={project.description.split(".")[0]}
+                projectUrl={project.projectUrl} 
+                onViewClick={()=>{setIsPopupVisible(true); setCurrrentProject(project)}}
+              />
+            ))
+          )
+        })
+      }
 
       {isPopupVisible? <Popup onOutsideClick={()=>{setIsPopupVisible(false)}} borderRadius={"2rem"} width={"50vw"} height={"80vh"}>
         <ProjectDetailScreen type={currrentProject.type} title={currrentProject.title} description={currrentProject.description} imageUrl={currrentProject.imageUrl} projectUrl={currrentProject.projectUrl} skills={currrentProject.skills} role={currrentProject.role}/>
